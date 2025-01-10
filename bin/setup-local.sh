b6e70568771ddbcdd8c9667b3c1665392752bff1
green-tmz <ya.ruslan2013@mail.ru>
2025-01-10 15:54:11 +0300

    Install PostGIS

--- bin/setup-local.sh
+++ bin/setup-local.sh
@@ -6,5 +6,6 @@ docker compose run --rm app php artisan storage:link
 docker compose up -d
 sleep 3
 docker compose exec app php artisan key:generate
-docker compose exec app php artisan migrate --seed
+docker compose exec pgsql psql -U organization -c "CREATE EXTENSION postgis;"
 docker compose exec pgsql psql -U organization -c "CREATE DATABASE organization_test;"
+docker compose exec app php artisan migrate --seed
